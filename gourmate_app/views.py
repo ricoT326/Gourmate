783bff01cc1ae6c994f7c5584380be4b30388ff7
Tai Ting Tsun Rico <2471034t@student.gla.ac.uk>
2021-04-04 23:45:01 +0100

    Added Recipe and Add Recipe views

--- gourmate_app/views.py
+++ gourmate_app/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from gourmate.forms import UserForm, UserProfileForm, CommentForm, RecipeForm
+from datetime import datetime
+from gourmate.models import Recipe, UserProfile, Comment
 
-# Create your views here.
+
+def index(request):
+    context_dict = {}
+    context_dict['visits'] = request.session['visits']
+    return render(request, 'gourmate/popular_recipes.html', context=context_dict)
+
+
+def restricted(request):
+    return render(request, 'gourmate/restricted.html')
+
+
+def recipe(request):
+    context_dict = {}
+    context_dict['Recipe'] = Recipe.objects.all()
+    recipe = Recipe.objects.all()
+    views = Recipe.views
+    return render(request, 'gourmate/recipe.html')
+
+@login_required
+def add_recipe(request):
+    if request.method == 'POST':
+        add_recipe = RecipeForm(request.POST)
+    return render(request, 'gourmate/add_recipe.html')
+
+
+
+
+'['
