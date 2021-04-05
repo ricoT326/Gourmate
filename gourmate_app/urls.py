23c1a0c30e7f33a6407c68ff1a96f43feecda0f1
Tai Ting Tsun Rico <2471034t@student.gla.ac.uk>
2021-04-05 01:52:00 +0100

    Fixed Typos and Added URLs

--- gourmate_app/urls.py
+++ gourmate_app/urls.py
@@ -1,16 +1,18 @@
 from django.urls import path
-from gourmate import views
+from gourmate_app import views
 
 app_name = 'gourmate'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('register/', views.register, name='register'),
-    path('login/', views.user_login, name='login'),
-    path('logout/', views.user_logout, name='logout'),
-    path('home/', views.home, name = 'home'),
-    path('logout/', views.user_logout, name='logout'),
-    path('contact us', views.contact_us, name='contact us'),
-    path('categories', views.categories, name = 'categories')
+    #path('register/', views.register, name='register'),
+    #path('login/', views.user_login, name='login'),
+    #path('logout/', views.user_logout, name='logout'),
+    #path('home/', views.home, name = 'home'),
+    #path('logout/', views.user_logout, name='logout'),
+    #path('contact-us/', views.contact_us, name='contact us'),
+    #path('chosen-recipe', views.chosen_recipe, name='chosen recipe'),
+    #path('categories/', views.categories, name = 'categories'),
+    
 ]
 
